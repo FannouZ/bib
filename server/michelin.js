1b9002576243a53a08a9a2164d46a4ecda4138be
FannouZ <59618653+FannouZ@users.noreply.github.com>
2020-01-21 21:48:53 +0100

    Update michelin.js

--- server/michelin.js
+++ server/michelin.js
@@ -9,9 +9,12 @@ const cheerio = require('cheerio');
 const parse = data => {
   const $ = cheerio.load(data);
   const name = $('.section-main h2.restaurant-details__heading--title').text();
+  const address = $('body > main > div.restaurant-details > div.container > div > div.col-xl-4.order-xl-8.col-lg-5.order-lg-7.restaurant-details__aside > div.restaurant-details__heading.d-lg-none > ul > li:nth-child(1)').text();
   const experience = $('#experience-section > ul > li:nth-child(2)').text();
+  const phone = $('body > main > div.restaurant-details > div.container > div > div.col-xl-8.col-lg-7 > section:nth-child(3) > div.row > div:nth-child(1) > div > div:nth-child(1) > div > div > a').attr('href');
+  const website = $('body > main > div.restaurant-details > div.container > div > div.col-xl-8.col-lg-7 > section:nth-child(3) > div.row > div:nth-child(1) > div > div.collapse__block-item.link-item > a').attr('href');
 
-  return {name, experience};
+  return {name, address, experience, phone, website};
 };
 
 /**
