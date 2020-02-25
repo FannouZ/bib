70f8a26d51e6b8ccecec38e59cc6cc4d8895a2ea
FannouZ <fannyzhg@gmail.com>
2020-02-25 13:05:53 +0100

    update

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,11 +1,39 @@
 /* eslint-disable no-console, no-process-exit */
 const michelin = require('./michelin');
 
-async function sandbox (searchLink = 'https://guide.michelin.com/fr/fr/centre-val-de-loire/veuves/restaurant/l-auberge-de-la-croix-blanche') {
-  try {
-    console.log(`🕵️‍♀️  browsing ${searchLink} source`);
 
-    const restaurant = await michelin.scrapeRestaurant(searchLink);
+/*faire une fonction qui appelle sandbox et qui modifie le lien a chaque fois
+liens d'un restaurant pour une page : div.col-md-6:nth-child(1) > div:nth-child(1) > a:nth-child(4).attr('href');*/
+async function listrestaurant (link ='https://guide.michelin.com/fr/fr/restaurants/bib-gourmand/page/1#'){
+	try{
+		for(var i=0;i<2;i++)
+		{
+			if (i!=9)
+			{
+				const rest_link= await michelin.scrapeLinks(link,i);
+				sandbox(rest_link);
+			}
+		}
+		/*
+		for(i=0; i<20;i++){
+			const restlink=michelin.scrapeRestaurant2(link);
+			//console.log(restlink);
+			sandbox(restlink);
+		}*/
+	}
+	catch(e){
+	    console.error(e);
+	    process.exit(1);
+	}
+	
+}
+
+/*modifier le parametre*/
+async function sandbox (searchLink) {
+  try {
+    console.log(`browsing ${searchLink} source`);
+
+    const restaurant = await michelin.scrapeRestaurant(searchLink,false);
 
     console.log(restaurant);
     console.log('done');
@@ -18,4 +46,4 @@ async function sandbox (searchLink = 'https://guide.michelin.com/fr/fr/centre-va
 
 const [,, searchLink] = process.argv;
 
-sandbox(searchLink);
+listrestaurant(searchLink);
